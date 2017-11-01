a94a1062bc64b4199903769ddc211c83261f84e1
H. Thomson Comer <thomcom@gmail.com>
2017-10-31 21:32:02 -0600

    Annotated d3_demo.js

--- d3_demo.js
+++ d3_demo.js
@@ -1,6 +1,7 @@
 var nodes = {};
 
 // Compute the distinct nodes from the links.
+// Creates nodes from edges if they don't already exist
 links.forEach(function(link) {
   link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
   link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
@@ -10,6 +11,11 @@ var width = 800,
     height = 400,
     radius = 16;
 
+// This is the d3 force layout - this uses the nodes and links specified
+// to run the d3.layout.force() function, automatically laying them out
+// visually and separating them.
+// The various parameters linkDistance, charge, chargeDistance, and gravity
+// change the spacing and speed with which the layout settles.
 var force = d3.layout.force()
     .nodes(d3.values(nodes))
     .links(links)
@@ -20,6 +26,7 @@ var force = d3.layout.force()
     .gravity(.007)
     .on("tick", tick);
 
+// Connect to the html document
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
@@ -27,10 +34,13 @@ var svg = d3.select("body").append("svg")
     console.log(force);
     console.log(nodes);
 
-nodes["ROOT"].x = width/3;
+// Fix the ROOT node to the center, not necessary with Dijkstra's
+nodes["ROOT"].x = width/2;
 nodes["ROOT"].y = height/2;
 nodes["ROOT"].fixed = true;
 
+// This looks up the "group" id from d3_demo_data.js and appends it to
+// the existing nodes. This is used for coloring and grouping.
 Object.keys(nodes).forEach(function(key) {
   console.log(key);
   console.log(nodes[key]);
@@ -40,8 +50,12 @@ Object.keys(nodes).forEach(function(key) {
   }
 });
 
+// Begin the simulation
 force.start();
 
+// All this specifies the layout of the edge. Tweaking it gets
+// slightly different edges.
+// Identifying Dijkstra's path would involve bolding these edges.
 // Per-type markers, as they don't inherit styles.
 svg.append("defs").selectAll("marker")
     .data(["link"])
@@ -54,25 +68,31 @@ svg.append("defs").selectAll("marker")
     .attr("markerHeight", 16)
     .attr("orient", "auto")
   .append("path")
-    .attr("d", "M-50,-25L-25,0L-50,25");
+    .attr("d", "M-50,-25L-25,0L-50,25"); // this weird code draws the arrow
 
+// Seems like this enables link clicking on the edges - it doesn't seem
+// to do anything and can probably be stripped.
 var path = svg.append("g").selectAll("path")
     .data(force.links())
   .enter().append("path")
     .attr("class", function(d) { return "link" })
     .attr("marker-end", function(d) { return "url(#link)"; });
 
+// This draws the edge label, great for displaying the weight
 var link_text = svg.append("g").selectAll("text")
     .data(force.links())
   .enter().append("text")
     .attr("class", "link-text")
     .text(function(d) { return d.label; });
 
+// This color object allows the circle fill function below to fill
+// each circle based on their group number.
 var color = d3.scale.category20();
 console.log(color);
 console.log(color(0));
 console.log(color(1));
 
+// Drawing code for each node
 var circle = svg.append("g").selectAll("circle")
     .data(force.nodes())
   .enter().append("circle")
@@ -80,6 +100,7 @@ var circle = svg.append("g").selectAll("circle")
     .attr("fill", function(d) { return color(d.group); })
     .call(force.drag);
 
+// Text labelling code for each node
 var text = svg.append("g").selectAll("text")
     .data(force.nodes())
   .enter().append("text")
@@ -87,6 +108,11 @@ var text = svg.append("g").selectAll("text")
     .attr("y", ".31em")
     .text(function(d) { return d.name; });
 
+// The tick function is called by d3.force during layout. The delta function
+// for each circle is modified as below for cx and cy, maybe enforcing that
+// circles don't overlap one another?
+// It also applies the linkArc function to each path element, the transform
+// function to text objects, and placeText function to link_text.
 // Use elliptical arc path segments to doubly-encode directionality.
 function tick() {
   path.attr("d", linkArc);
@@ -96,6 +122,7 @@ function tick() {
   link_text.attr("transform", placeText);
 }
 
+// line goes from target to source in x and y, below is an affine matrix
 function linkArc(d) {
   var dx = d.target.x - d.source.x,
       dy = d.target.y - d.source.y,
@@ -103,16 +130,20 @@ function linkArc(d) {
   return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + (d.target.y);
 }
 
+// Put the link text in the middle of each edge
 function placeText(d) {
   var dx = (d.target.px - d.source.px)/2 + d.source.px,
       dy = (d.target.py - d.source.py)/2 + d.source.py;
   return "translate(" + dx + "," + dy + ")";
 }
 
+// write the text where the circle is
 function transform(d) {
   return "translate(" + d.x + "," + d.y + ")";
 }
 
+// This reloads the data from d3_demo_data.js, then reruns the force code.
+// This would be fly for redrawing new Dijkstra's nodes.
 var updateData = function() {
   links = d3.json('d3_demo_data.js');
   force.resume();
